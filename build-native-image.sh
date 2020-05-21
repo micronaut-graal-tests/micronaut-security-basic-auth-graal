8647c7117c3812b6da98bacb51c969c0f1c6bbb9
Iván López <lopezi@objectcomputing.com>
2020-05-21 11:22:55 +0200

    Remove --static when building native-image

--- build-native-image.sh
+++ build-native-image.sh
@@ -1,2 +1,2 @@
 ./gradlew assemble
-native-image --no-server --static --no-fallback --class-path build/libs/micronaut-security-basicauth-graal-0.1-all.jar
+native-image --no-server --no-fallback --class-path build/libs/micronaut-security-basicauth-graal-0.1-all.jar
